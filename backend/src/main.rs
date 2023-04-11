8cfe6582db2922826c34a273200c73e51889f1ff
DevCorvus <devcorvus.contact@gmail.com>
2023-04-11 18:57:43 -0400

    feat: actix-web server setup with healthcheck

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/health")]
+async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(healthcheck))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
 }
