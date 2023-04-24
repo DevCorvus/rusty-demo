a045f76004e8482b38a1b29ac6c832da168fd22e
DevCorvus <devcorvus.contact@gmail.com>
2023-04-24 17:11:49 -0400

    feat: check if user exists (service)

--- backend/src/users/service.rs
+++ backend/src/users/service.rs
@@ -1,5 +1,8 @@
 use crate::database::DbError;
-use diesel::prelude::*;
+use diesel::{
+    dsl::{exists, select},
+    prelude::*,
+};
 
 use super::{dto::CreateUserDto, model::User};
 
@@ -25,4 +28,11 @@ pub fn add_user(conn: &mut SqliteConnection, data: &CreateUserDto) -> Result<Use
     let new_user = users.filter(email.eq(&data.email)).first::<User>(conn)?;
 
     Ok(new_user)
+}
+
+pub fn check_if_user_exists(conn: &mut SqliteConnection, user_id: i32) -> Result<bool, DbError> {
+    use crate::schema::users::dsl::*;
+    let user_exists = select(exists(users.find(user_id))).get_result::<bool>(conn)?;
+
+    Ok(user_exists)
 }
