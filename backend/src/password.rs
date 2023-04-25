5142c76f05fc0db7f86e61d7073763e714ec6804
DevCorvus <devcorvus.contact@gmail.com>
2023-04-25 15:00:29 -0400

    fix: replace custom errors for password hash/compare with anyhow errors and include tests

--- backend/src/password.rs
+++ backend/src/password.rs
@@ -1,42 +1,53 @@
-use thiserror::Error;
+use anyhow;
 
 use argon2::{
     password_hash::{rand_core::OsRng, SaltString},
     Argon2, PasswordHash, PasswordHasher, PasswordVerifier,
 };
 
-#[derive(Error, Debug)]
-#[error("Error hashing password")]
-pub struct PasswordHashError;
-
-#[derive(Error, Debug)]
-#[error("Error comparing passwords")]
-pub struct PasswordCompareError;
-
-pub fn hash_password(password: String) -> Result<String, PasswordHashError> {
+pub fn hash_password(password: String) -> anyhow::Result<String> {
     let argon2 = Argon2::default();
 
     let salt = SaltString::generate(&mut OsRng);
     let password_hash = argon2
         .hash_password(password.as_bytes(), &salt)
-        .map_err(|_| PasswordHashError)?
+        .map_err(anyhow::Error::msg)?
         .to_string();
 
     Ok(password_hash)
 }
 
-pub fn compare_password(
-    password: String,
-    password_hash: String,
-) -> Result<bool, PasswordCompareError> {
+pub fn compare_password(password: String, password_hash: String) -> anyhow::Result<bool> {
     let argon2 = Argon2::default();
 
-    let pased_password_hash =
-        PasswordHash::new(&password_hash).map_err(|_| PasswordCompareError)?;
+    let pased_password_hash = PasswordHash::new(&password_hash).map_err(anyhow::Error::msg)?;
 
     let do_passwords_match = argon2
         .verify_password(password.as_bytes(), &pased_password_hash)
         .is_ok();
 
     Ok(do_passwords_match)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hash_password() -> anyhow::Result<()> {
+        let password = String::from("123456");
+        let password_hash = hash_password(password.clone())?;
+        assert_ne!(password, password_hash);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_compare_password() -> anyhow::Result<()> {
+        let password = String::from("123456");
+        let password_hash = hash_password(password.clone())?;
+        assert!(compare_password(password, password_hash)?);
+
+        Ok(())
+    }
 }
