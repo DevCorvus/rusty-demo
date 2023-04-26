824fd240fb54cc4e2d42638f69cb87785395f71f
DevCorvus <devcorvus.contact@gmail.com>
2023-04-26 14:46:08 -0400

    feat: get user profile and update/delete user (endpoints) with new dtos and user output filtering

--- backend/src/users/dto.rs
+++ backend/src/users/dto.rs
@@ -1,11 +1,25 @@
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
 use crate::schema::users;
 
-#[derive(Serialize, Deserialize, Insertable)]
+#[derive(Deserialize, Insertable)]
 #[diesel(table_name = users)]
 pub struct CreateUserDto {
     pub email: String,
     pub password: String,
+}
+
+#[derive(Deserialize)]
+pub struct UpdateUserPasswordDto {
+    pub password: String,
+}
+
+#[derive(Serialize)]
+pub struct FilteredUserDto {
+    pub id: i32,
+    pub email: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
 }
--- backend/src/users/handlers.rs
+++ backend/src/users/handlers.rs
@@ -1,13 +1,34 @@
 use crate::password;
 use crate::{auth::middleware, database::DbPool};
 
-use super::{dto::CreateUserDto, service};
+use super::{
+    dto::CreateUserDto,
+    dto::{FilteredUserDto, UpdateUserPasswordDto},
+    model::User,
+    service,
+};
 use crate::error::AppError;
 use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
 
 #[get("/profile")]
-async fn get_user_profile(_: middleware::JwtMiddleware) -> impl Responder {
-    HttpResponse::Ok().body("User profile")
+async fn get_user_profile(
+    pool: web::Data<DbPool>,
+    ctx: middleware::JwtMiddleware,
+) -> actix_web::Result<impl Responder> {
+    let user = web::block(move || {
+        let mut conn = pool.get().map_err(|_| AppError::InternalError)?;
+
+        let user_found =
+            service::get_user_by_id(&mut conn, ctx.user_id).map_err(|_| AppError::InternalError)?;
+
+        match user_found {
+            Some(u) => Ok(u),
+            None => Err(AppError::NotFound(String::from("User"))),
+        }
+    })
+    .await??;
+
+    Ok(HttpResponse::Ok().json(filter_user_output(&user)))
 }
 
 #[post("/")]
@@ -39,24 +60,55 @@ async fn create_user(
     })
     .await??;
 
-    Ok(HttpResponse::Created().json(new_user))
+    Ok(HttpResponse::Created().json(filter_user_output(&new_user)))
 }
 
 #[put("/")]
-async fn update_user(_: middleware::JwtMiddleware) -> impl Responder {
-    HttpResponse::Ok().body("Update user")
+async fn update_user_password(
+    pool: web::Data<DbPool>,
+    data: web::Json<UpdateUserPasswordDto>,
+    ctx: middleware::JwtMiddleware,
+) -> actix_web::Result<impl Responder> {
+    web::block(move || {
+        let mut conn = pool.get()?;
+        let password_hash = password::hash_password(data.password.clone())?;
+        service::update_user_password(&mut conn, ctx.user_id, password_hash)
+    })
+    .await?
+    .map_err(|_| AppError::InternalError)?;
+
+    Ok(HttpResponse::Ok().body("User password updated successfully"))
 }
 
 #[delete("/")]
-async fn delete_user(_: middleware::JwtMiddleware) -> impl Responder {
-    HttpResponse::Ok().body("Delete user")
+async fn delete_user(
+    pool: web::Data<DbPool>,
+    ctx: middleware::JwtMiddleware,
+) -> actix_web::Result<impl Responder> {
+    web::block(move || {
+        let mut conn = pool.get()?;
+        service::delete_user(&mut conn, ctx.user_id)
+    })
+    .await?
+    .map_err(|_| AppError::InternalError)?;
+
+    Ok(HttpResponse::Ok().body("User deleted successfully"))
+}
+
+fn filter_user_output(user: &User) -> FilteredUserDto {
+    FilteredUserDto {
+        id: user.id,
+        email: user.email.to_owned(),
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+    }
 }
 
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/users")
         .service(get_user_profile)
         .service(create_user)
-        .service(update_user)
+        .service(update_user_password)
         .service(delete_user);
 
     conf.service(scope);
