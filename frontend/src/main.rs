015024f8495d6b51588a2afec00f514e2d0f5e45
DevCorvus <devcorvus.contact@gmail.com>
2023-04-27 12:07:03 -0400

    feat: yew setup with tailwindcss

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div class={classes!("text-orange-500", "text-3xl")}>
+            <h1>{ "Rusty demo" }</h1>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
