b831847465bb4beb8aea0131ff0dd1cf122412be
DevCorvus <devcorvus.contact@gmail.com>
2023-05-01 20:31:52 -0400

    feat: show authentication status on home page

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -1,10 +1,23 @@
 use yew::prelude::*;
+use yewdux::prelude::use_store;
+
+use crate::store::Store;
 
 #[function_component(Home)]
 pub fn home() -> Html {
+    let (store, _) = use_store::<Store>();
+
     html! {
-        <div class="text-2xl">
-            <h1>{ "Home" }</h1>
+        <div class="text-2xl text-slate-100">
+            {if store.token.is_some() {
+                html! {
+                    <h1>{ "You are authenticated!" }</h1>
+                }
+            } else {
+                html! {
+                    <h1>{ "You are not authenticated .(" }</h1>
+                }
+            }}
         </div>
     }
 }
